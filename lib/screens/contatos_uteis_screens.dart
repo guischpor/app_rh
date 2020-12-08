a6a90e057349a70ad873bc8e0caeea0d15d0a41f
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2020-12-08 20:12:04 -0300

    build card contatos

--- lib/screens/contatos_uteis_screens.dart
+++ lib/screens/contatos_uteis_screens.dart
@@ -1,4 +1,5 @@
 import 'package:app_rh/styles/styles.dart';
+import 'package:app_rh/widgets/menu_drawer/build_card.contatos.dart';
 import 'package:flutter/material.dart';
 
 class ContatosUteisScreen extends StatefulWidget {
@@ -20,6 +21,63 @@ class _ContatosUteisScreenState extends State<ContatosUteisScreen> {
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
         iconTheme: IconThemeData(color: styles.iconColorBlue),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            BuildCardContatos(
+              iconData: Icons.code,
+              iconColor: styles.iconColorBlue,
+              iconSize: 70,
+              textSize: 15,
+              gerente: "Aldair Santos",
+              setor: "Fábrica de Software",
+              email: "fabsoft@email.com",
+              phone: "(41) 3333-2222",
+            ),
+            BuildCardContatos(
+              iconData: Icons.person,
+              iconColor: styles.iconColorBlue,
+              iconSize: 70,
+              textSize: 15,
+              gerente: "Fabiola Padilha",
+              setor: "Recursos Humanos",
+              email: "rht@email.com",
+              phone: "(41) 3333-2233",
+            ),
+            BuildCardContatos(
+              iconData: Icons.insert_drive_file,
+              iconColor: styles.iconColorBlue,
+              iconSize: 70,
+              textSize: 15,
+              gerente: "Simone Dantas",
+              setor: "Administração",
+              email: "adm@email.com",
+              phone: "(41) 3333-2244",
+            ),
+            BuildCardContatos(
+              iconData: Icons.person_outline,
+              iconColor: styles.iconColorBlue,
+              iconSize: 70,
+              textSize: 15,
+              gerente: "Miguel Souza",
+              setor: "Diretoria",
+              email: "diretoria@email.com",
+              phone: "(41) 3333-2255",
+            ),
+            BuildCardContatos(
+              iconData: Icons.build,
+              iconColor: styles.iconColorBlue,
+              iconSize: 70,
+              textSize: 15,
+              gerente: "Leo Paiva",
+              setor: "Manutenção de TI",
+              email: "manutti@email.com",
+              phone: "(41) 3333-2266",
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/build_flat_button.dart
+++ lib/widgets/build_flat_button.dart
@@ -11,7 +11,7 @@ class BuildFlatButton extends StatelessWidget {
     return FlatButton(
       onPressed: onPressed,
       textColor: textColor,
-      child: Text(title, style: TextStyle(letterSpacing: 0.6, fontSize: 11)),
+      child: Text(title, style: TextStyle(letterSpacing: 0.2, fontSize: 11)),
     );
   }
 }
