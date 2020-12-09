e210720b099957d149327eda347d3fa8c7f7431c
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2020-12-09 20:39:45 -0300

    up

---
+++ lib/models/ContatoModel.dart
@@ -0,0 +1,36 @@
+class ContatoModel {
+  int id;
+  String icon;
+  String gerente;
+  String setor;
+  String email;
+  String telefone;
+
+  ContatoModel(
+      {this.id,
+      this.icon,
+      this.gerente,
+      this.setor,
+      this.email,
+      this.telefone});
+
+  ContatoModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    icon = json['icon'];
+    gerente = json['gerente'];
+    setor = json['setor'];
+    email = json['email'];
+    telefone = json['telefone'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['icon'] = this.icon;
+    data['gerente'] = this.gerente;
+    data['setor'] = this.setor;
+    data['email'] = this.email;
+    data['telefone'] = this.telefone;
+    return data;
+  }
+}
--- lib/screens/contatos_uteis_screens.dart
+++ lib/screens/contatos_uteis_screens.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:app_rh/styles/styles.dart';
 import 'package:app_rh/widgets/build_card.contatos.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:app_rh/models/ContatoModel.dart';
 
 class ContatosUteisScreen extends StatefulWidget {
   @override
@@ -9,6 +13,7 @@ class ContatosUteisScreen extends StatefulWidget {
 
 class _ContatosUteisScreenState extends State<ContatosUteisScreen> {
   Styles styles = Styles();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,62 +28,29 @@ class _ContatosUteisScreenState extends State<ContatosUteisScreen> {
         iconTheme: IconThemeData(color: styles.iconColorBlue),
       ),
       body: Container(
-        padding: EdgeInsets.all(16),
-        child: ListView(
-          children: <Widget>[
-            BuildCardContatos(
-              iconData: Icons.code,
-              iconColor: styles.iconColorBlue,
-              iconSize: 70,
-              textSize: 15,
-              gerente: "Aldair Santos",
-              setor: "Fábrica de Software",
-              email: "fabsoft@email.com",
-              phone: "(41) 3333-2222",
-            ),
-            BuildCardContatos(
-              iconData: Icons.person,
-              iconColor: styles.iconColorBlue,
-              iconSize: 70,
-              textSize: 15,
-              gerente: "Fabiola Padilha",
-              setor: "Recursos Humanos",
-              email: "rht@email.com",
-              phone: "(41) 3333-2233",
-            ),
-            BuildCardContatos(
-              iconData: Icons.insert_drive_file,
-              iconColor: styles.iconColorBlue,
-              iconSize: 70,
-              textSize: 15,
-              gerente: "Simone Dantas",
-              setor: "Administração",
-              email: "adm@email.com",
-              phone: "(41) 3333-2244",
-            ),
-            BuildCardContatos(
-              iconData: Icons.person_outline,
-              iconColor: styles.iconColorBlue,
-              iconSize: 70,
-              textSize: 15,
-              gerente: "Miguel Souza",
-              setor: "Diretoria",
-              email: "diretoria@email.com",
-              phone: "(41) 3333-2255",
-            ),
-            BuildCardContatos(
-              iconData: Icons.build,
-              iconColor: styles.iconColorBlue,
-              iconSize: 70,
-              textSize: 15,
-              gerente: "Leo Paiva",
-              setor: "Manutenção de TI",
-              email: "manutti@email.com",
-              phone: "(41) 3333-2266",
-            ),
-          ],
-        ),
-      ),
+          padding: EdgeInsets.all(16),
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+              return ListView.builder(
+                itemCount: showData.length,
+                itemBuilder: (context, index) {
+                  return BuildCardContatos(
+                    iconData: Icons.code,
+                    iconColor: styles.iconColorBlue,
+                    iconSize: 70,
+                    textSize: 15,
+                    gerente: showData[index]["gerente"],
+                    setor: showData[index]["setor"],
+                    email: showData[index]["email"],
+                    phone: showData[index]["telefone"],
+                  );
+                },
+              );
+            },
+            future: DefaultAssetBundle.of(context)
+                .loadString('assets/json/contatos.json'),
+          )),
     );
   }
 }
