825f05c96b632c51c93f04ec03576eec3fc4314c
Gui <guischpor1@live.com>
2020-12-17 16:12:19 -0300

    build Alert Dialog

--- lib/screens/esqueceu_senha_screen.dart
+++ lib/screens/esqueceu_senha_screen.dart
@@ -1,6 +1,7 @@
 import 'package:app_rh/styles/styles.dart';
 import 'package:app_rh/widgets/build_button.dart';
 import 'package:app_rh/widgets/input_field.dart';
+import 'package:app_rh/widgets/title_page.dart';
 import 'package:flutter/material.dart';
 
 class EsqueceuSenhaScreen extends StatefulWidget {
@@ -27,42 +28,117 @@ class _EsqueceuSenhaScreenState extends State<EsqueceuSenhaScreen> {
         iconTheme: IconThemeData(color: styles.textColorBlue),
       ),
       body: Container(
-          width: mediaQuery.size.width,
-          height: mediaQuery.size.height,
-          child: LayoutBuilder(
-            builder: (_, constrains) {
-              return ListView(
-                padding: EdgeInsets.all(16),
-                children: [
-                  SizedBox(
-                    height: constrains.maxHeight * 0.10,
+        width: mediaQuery.size.width,
+        height: mediaQuery.size.height,
+        child: LayoutBuilder(
+          builder: (_, constrains) {
+            return ListView(
+              padding: EdgeInsets.all(16),
+              children: [
+                SizedBox(
+                  height: constrains.maxHeight * 0.01,
+                ),
+                Container(
+                  padding: EdgeInsets.all(30),
+                  child: TitlePage(
+                    title: "Por favor, digite o seu endereço de e-mail.",
+                    color: styles.textColorBlue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    textAlign: TextAlign.center,
                   ),
-                  InputField(
-                    hint: "Email",
-                    preffix: Icon(
-                      Icons.email,
-                      //color: styles.iconColorGrey,
-                    ),
-                    textInputType: TextInputType.text,
-                    onChanged: (value) => {},
-                    obscure: false,
-                    enable: true,
+                ),
+                InputField(
+                  hint: "Email",
+                  preffix: Icon(
+                    Icons.email,
+                    //color: styles.iconColorGrey,
                   ),
-                  SizedBox(
-                    height: 15,
+                  textInputType: TextInputType.text,
+                  onChanged: (value) => {},
+                  obscure: false,
+                  enable: true,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 50,
+                  child: BuildButton(
+                    colorButton: styles.colorButtons,
+                    onPressed: () => {_showAlerta(context)},
+                    title: "ENVIAR SENHA",
                   ),
-                  Container(
-                    height: 50,
-                    child: BuildButton(
-                      colorButton: styles.colorButtons,
-                      onPressed: () => {},
-                      title: "ENVIAR SENHA",
-                    ),
-                  ),
-                ],
-              );
-            },
-          )),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
+  }
+
+  void _showAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          content: Container(
+            height: 330,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.lock,
+                  size: 130,
+                  color: styles.iconColorBlue,
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: TitlePage(
+                    title: "Esqueceu a sua senha, não se preocupe",
+                    color: styles.textColorAlert,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TitlePage(
+                    title: "Você receberá a sua senha de acesso no seu email.",
+                    color: styles.iconColorGrey,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 50,
+                  margin: EdgeInsets.only(top: 15),
+                  child: BuildButton(
+                    colorButton: styles.colorButtons,
+                    onPressed: _routeLoginPage,
+                    title: "OK",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _routeLoginPage() async {
+    Navigator.pushNamed(context, 'login');
   }
 }
--- lib/screens/solicitar_acesso_screen.dart
+++ lib/screens/solicitar_acesso_screen.dart
@@ -1,6 +1,7 @@
 import 'package:app_rh/styles/styles.dart';
 import 'package:app_rh/widgets/build_button.dart';
 import 'package:app_rh/widgets/input_field.dart';
+import 'package:app_rh/widgets/title_page.dart';
 import 'package:flutter/material.dart';
 
 class SolicitarAcessoScreen extends StatefulWidget {
@@ -27,70 +28,136 @@ class _SolicitarAcessoScreenState extends State<SolicitarAcessoScreen> {
         iconTheme: IconThemeData(color: styles.textColorBlue),
       ),
       body: Container(
-          width: mediaQuery.size.width,
-          height: mediaQuery.size.height,
-          child: LayoutBuilder(
-            builder: (_, constrains) {
-              return ListView(
-                padding: EdgeInsets.all(16),
-                children: [
-                  SizedBox(
-                    height: constrains.maxHeight * 0.10,
+        width: mediaQuery.size.width,
+        height: mediaQuery.size.height,
+        child: LayoutBuilder(
+          builder: (_, constrains) {
+            return ListView(
+              padding: EdgeInsets.all(16),
+              children: [
+                SizedBox(
+                  height: constrains.maxHeight * 0.10,
+                ),
+                InputField(
+                  hint: "Matrícula",
+                  preffix: Icon(
+                    Icons.vpn_key,
+                    //color: styles.iconColorGrey,
                   ),
-                  InputField(
-                    hint: "Matrícula",
-                    preffix: Icon(
-                      Icons.vpn_key,
-                      //color: styles.iconColorGrey,
-                    ),
-                    textInputType: TextInputType.text,
-                    onChanged: (value) => {},
-                    obscure: false,
-                    enable: true,
+                  textInputType: TextInputType.text,
+                  onChanged: (value) => {},
+                  obscure: false,
+                  enable: true,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                InputField(
+                  hint: "Nome",
+                  preffix: Icon(
+                    Icons.person,
+                    //color: styles.iconColorGrey,
                   ),
-                  SizedBox(
-                    height: 15,
+                  textInputType: TextInputType.text,
+                  onChanged: (value) => {},
+                  obscure: false,
+                  enable: true,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                InputField(
+                  hint: "Email",
+                  preffix: Icon(
+                    Icons.email,
+                    //color: styles.iconColorGrey,
                   ),
-                  InputField(
-                    hint: "Nome",
-                    preffix: Icon(
-                      Icons.person,
-                      //color: styles.iconColorGrey,
-                    ),
-                    textInputType: TextInputType.text,
-                    onChanged: (value) => {},
-                    obscure: false,
-                    enable: true,
+                  textInputType: TextInputType.text,
+                  onChanged: (value) => {},
+                  obscure: false,
+                  enable: true,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 50,
+                  child: BuildButton(
+                    colorButton: styles.colorButtons,
+                    onPressed: () => {_showAlerta(context)},
+                    title: "SOLICITAR",
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  InputField(
-                    hint: "Email",
-                    preffix: Icon(
-                      Icons.email,
-                      //color: styles.iconColorGrey,
-                    ),
-                    textInputType: TextInputType.text,
-                    onChanged: (value) => {},
-                    obscure: false,
-                    enable: true,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    height: 50,
-                    child: BuildButton(
-                      colorButton: styles.colorButtons,
-                      onPressed: () => {},
-                      title: "SOLICITAR",
-                    ),
-                  ),
-                ],
-              );
-            },
-          )),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
+  }
+
+  void _showAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          content: Container(
+            height: 330,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.account_circle,
+                  size: 130,
+                  color: styles.iconColorBlue,
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: TitlePage(
+                    title: "Você acabou de solicitar o acesso a sua conta ",
+                    color: styles.textColorAlert,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TitlePage(
+                    title:
+                        "Você receberá as informações e a senha de acesso no seu email.",
+                    color: styles.iconColorGrey,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 50,
+                  margin: EdgeInsets.only(top: 15),
+                  child: BuildButton(
+                    colorButton: styles.colorButtons,
+                    onPressed: _routeLoginPage,
+                    title: "OK",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _routeLoginPage() async {
+    Navigator.pushNamed(context, 'login');
   }
 }
